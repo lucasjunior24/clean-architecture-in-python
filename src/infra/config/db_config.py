2ebfe5ea0d14b2b8bd031c6e3718231fe53d47dc
lucas souza <lucassousa@setic.ro.gov.br>
2022-12-24 11:45:15 -0400

    feat: implementing infra with config, entities and repo

--- src/infra/config/db_config.py
+++ src/infra/config/db_config.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
 
 class DbConnectionHandler:
     """Sqlalchemy  """
@@ -13,3 +14,12 @@ class DbConnectionHandler:
         """
         engine = create_engine(self.__connection_string)
         return engine
+
+    def __enter__(self):
+        engine = create_engine(self.__connection_string)
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close() # pylint: disable=no=member
